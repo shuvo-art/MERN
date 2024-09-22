1c55a8decde6f368116f98a4c10797cb37bd8300
shuvo-art <68417858+shuvo-art@users.noreply.github.com>
2024-09-22 17:03:49 +0600

    Update server.js

--- disaster-management-backend/server.js
+++ disaster-management-backend/server.js
@@ -25,7 +25,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://deploy-mern-frontend-rouge.vercel.app", // Allow requests from frontend port
+    origin: "https://mern-frontend-psi-gules.vercel.app", // Allow requests from frontend port
   })
 );
 app.use(express.json());
